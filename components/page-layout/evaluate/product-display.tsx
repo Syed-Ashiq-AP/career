0a77444d9ef68007a169c95800fcab9fef918e5a
Syed-Ashiq-AP <syedashiq.ap@gmail.com>
2025-12-27 10:28:01 +0530

    tsconfig

--- components/page-layout/evaluate/product-display.tsx
+++ components/page-layout/evaluate/product-display.tsx
@@ -6,39 +6,39 @@ import { redirect } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 type Props = {
-    children: React.ReactNode;
-    session: { session: Session; user: User };
+  children: React.ReactNode;
+  session: { session: Session; user: User };
 };
 
 const ProductDisplay = ({ children, session }: Props) => {
-    const [status, setStatus] = useState(0);
+  const [status, setStatus] = useState(0);
 
-    useEffect(() => {
-        if (!session) return;
-        const fetch = async () => {
-            const { data } = await authClient.customer.orders.list({
-                query: {
-                    page: 1,
-                    limit: 10,
-                },
-            });
-            if (!data) return;
-            const { items } = data.result;
-            const orders = items.map((item: any) => item.productId);
-            if (orders.length === 0) setStatus(-1);
-            else setStatus(1);
-        };
-        fetch();
-    }, [session]);
+  useEffect(() => {
+    if (!session) return;
+    const fetch = async () => {
+      const { data } = await authClient.customer.orders.list({
+        query: {
+          page: 1,
+          limit: 10,
+        },
+      });
+      if (!data) return;
+      const { items } = data.result;
+      const orders = items.map((item: any) => item.productId);
+      if (orders.length === 0) setStatus(-1);
+      else setStatus(1);
+    };
+    fetch();
+  }, [session]);
 
-    if (!status)
-        return (
-            <div className="w-full h-full flex justify-center items-center">
-                <p>Setting Up...</p>
-            </div>
-        );
-    else if (status === -1) redirect("/checkout");
-    else if (status === 1) return children;
+  if (!status)
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <p>Setting Up...</p>
+      </div>
+    );
+  else if (status === -1) redirect("/checkout");
+  else if (status === 1) return children;
 };
 
 export default ProductDisplay;
