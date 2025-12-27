f8a5a97d0120044a776cdf07f74ba311cc6527f1
Syed-Ashiq-AP <syedashiq.ap@gmail.com>
2025-12-27 10:42:28 +0530

    redirect to auth

--- app/evaluate/page.tsx
+++ app/evaluate/page.tsx
@@ -4,22 +4,23 @@ import { EvaluateContextProvider } from "@/components/providers/career-evaluatio
 import { ChatBotProvider } from "@/hooks/use-chat";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-    const session = await auth.api.getSession({ headers: await headers() });
+  const session = await auth.api.getSession({ headers: await headers() });
 
-    if (!session) return;
+  if (!session) redirect("/auth/sign-in");
 
-    return (
-        <ChatBotProvider userId={session?.user.id}>
-            <EvaluateContextProvider>
-                <ProductDisplay session={session}>
-                    <AiView />
-                </ProductDisplay>
-            </EvaluateContextProvider>
-        </ChatBotProvider>
-    );
+  return (
+    <ChatBotProvider userId={session?.user.id}>
+      <EvaluateContextProvider>
+        <ProductDisplay session={session}>
+          <AiView />
+        </ProductDisplay>
+      </EvaluateContextProvider>
+    </ChatBotProvider>
+  );
 };
 
 export default Page;
